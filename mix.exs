e8e009f1a4b29b567d8122464aa6707f8d12b033
Mathijs Saey <mathsaey@vub.ac.be>
2018-03-07 18:24:04 +0100

    Remove global dependencies

--- mix.exs
+++ mix.exs
@@ -5,16 +5,7 @@ defmodule Skitter.MixProject do
     [
       apps_path: "apps",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: []
     ]
-  end
-
-  # Dependencies listed here are available only for this
-  # project and cannot be accessed from applications inside
-  # the apps folder.
-  #
-  # Run "mix help deps" for examples and options.
-  defp deps do
-    []
   end
 end
