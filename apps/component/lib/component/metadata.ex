516cccc21a2b2a4ffc76ff159c6518fd58975aaf
Mathijs Saey <mathsaey@vub.ac.be>
2018-08-15 21:01:57 +0200

    Remove unused type field

--- apps/component/lib/component/metadata.ex
+++ apps/component/lib/component/metadata.ex
@@ -1,14 +1,13 @@
 defmodule Skitter.Component.Metadata do
   @moduledoc false
-  @data [:name, :description, :effects, :in_ports, :out_ports, :in_ports_size]
+  @data [:name, :description, :effects, :in_ports, :out_ports]
 
   @type t :: %__MODULE__{
           name: String.t(),
           description: String.t(),
           effects: [keyword()],
           in_ports: [atom()],
-          out_ports: [atom()],
-          in_ports_size: integer()
+          out_ports: [atom()]
         }
 
   @enforce_keys @data
