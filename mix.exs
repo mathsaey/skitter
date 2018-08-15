f349973abd122dcdb65cf10b1d89fa8324d6a4ed
Mathijs Saey <mathsaey@vub.ac.be>
2018-08-15 16:43:39 +0200

    Add release support

--- mix.exs
+++ mix.exs
@@ -13,6 +13,7 @@ defmodule Skitter.MixProject do
   defp deps do
     [
       # Dev tools
+      {:distillery, "~> 2.0", runtime: false},
       {:ex_doc, "~> 0.16", only: :dev, runtime: false},
       {:credo, "~> 0.9.1", only: [:dev], runtime: false},
       {:dialyxir, "~> 0.5", only: [:dev], runtime: false}
