d74de3afd68a3509fb7a789392ec3d319510c816
Mathijs Saey <mathsaey@vub.ac.be>
2018-09-19 16:21:54 +0200

    Added license

--- lib/component/behaviour.ex
+++ lib/component/behaviour.ex
@@ -1,3 +1,9 @@
+# Copyright 2018, Mathijs Saey, Vrije Universiteit Brussel
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 defmodule Skitter.Component.Behaviour do
   @moduledoc false
 
