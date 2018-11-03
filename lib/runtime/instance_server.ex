127bfa01f5c1b7d7ebd9346d3564dbd3b6cf266a
Mathijs Saey <mathsaey@vub.ac.be>
2018-11-03 12:05:01 -0400

    Move GenServer and api into same module
    - Keeping them separate does not work nicely with child specs

--- lib/runtime/instance_server.ex
+++ lib/runtime/instance_server.ex
@@ -7,49 +7,53 @@
 defmodule Skitter.Runtime.InstanceServer do
   @moduledoc false
 
+  use GenServer
+  require Logger
+
+  # --- #
+  # API #
+  # --- #
+
   def start_link(comp, init, id \\ nil) do
-    GenServer.start_link(__MODULE__.Server, {comp, init, id})
+    GenServer.start_link(__MODULE__, {comp, init, id})
   end
 
   def react(srv, args, timeout \\ :infinity) do
     GenServer.call(srv, {:react, args}, timeout)
   end
 
-  defmodule Server do
-    @moduledoc false
+  # ------ #
+  # Server #
+  # ------ #
 
-    require Logger
-    use GenServer
+  def init({comp, init, id}) do
+    setup_logger(comp, id)
+    Logger.debug "Started instance server"
+    {:ok, nil, {:continue, {comp, init}}}
+  end
 
-    def init({comp, init, id}) do
-      setup_logger(comp, id)
-      Logger.info "Started instance server"
-      {:ok, nil, {:continue, {comp, init}}}
-    end
+  def handle_continue({comp, init}, nil) do
+    {:ok, instance} = Skitter.Component.init(comp, init)
+    Logger.debug "Finished initialization", state: inspect(instance.state)
+    {:noreply, instance}
+  end
 
-    def handle_continue({comp, init}, nil) do
-      {:ok, instance} = Skitter.Component.init(comp, init)
-      Logger.debug "Finished initialization", state: inspect(instance.state)
-      {:noreply, instance}
-    end
+  defp setup_logger(comp, id) do
+    metadata = [
+      identifier: id,
+      component: comp,
+    ]
 
-    def handle_call({:react, args}, _, instance) do
-      Logger.debug "React", args: inspect(args), state: inspect(instance.state)
-      {:ok, instance, spits} = Skitter.Component.react(instance, args)
-      Logger.debug "Finished reacting", state: inspect(instance.state)
-      {:reply, {:ok, spits}, instance}
-    end
+    keys = [:pid] ++ Keyword.keys(metadata) ++ [:state, :args]
 
-    defp setup_logger(comp, id) do
-      metadata = [
-        identifier: id,
-        component: comp,
-      ]
+    Logger.metadata(metadata)
+    Logger.configure_backend(:console, metadata: keys)
+  end
 
-      keys = [:pid] ++ Keyword.keys(metadata) ++ [:state, :args]
-
-      Logger.metadata(metadata)
-      Logger.configure_backend(:console, metadata: keys)
-    end
+  def handle_call({:react, args}, _, instance) do
+    Logger.debug "React", args: inspect(args), state: inspect(instance.state)
+    {:ok, instance, spits} = Skitter.Component.react(instance, args)
+    Logger.debug "Finished reacting", state: inspect(instance.state)
+    {:reply, {:ok, spits}, instance}
   end
 end
