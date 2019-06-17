61aee08ce1671558681235d56eab499d84aedd96
Mathijs Saey <mathsaey@vub.ac.be>
2019-06-17 21:56:32 +0200

    Add syntax error

---
+++ lib/syntax_error.ex
@@ -0,0 +1,24 @@
+# Copyright 2018, 2019 Mathijs Saey, Vrije Universiteit Brussel
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+defmodule Skitter.SyntaxError do
+  @moduledoc """
+  This error is raised when invalid syntax was encountered in a Skitter DSL.
+  """
+  defexception [:message, :env]
+
+  @impl true
+  def message(%__MODULE__{message: msg, env: nil}) do
+    msg
+  end
+
+  def message(%__MODULE__{message: msg, env: env}) do
+    loc = Exception.format_file_line(env.file, env.line, " ")
+    loc <> msg
+  end
+
+  def raise(m, e \\ nil), do: Kernel.raise(__MODULE__, message: m, env: e)
+end
