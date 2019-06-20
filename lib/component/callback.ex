0b92ec5d57acc392089c7b72774aab63c07ac940
Mathijs Saey <mathsaey@vub.ac.be>
2019-06-20 14:32:37 +0200

    Create defcallback macro

--- lib/component/callback.ex
+++ lib/component/callback.ex
@@ -11,9 +11,11 @@ defmodule Skitter.Component.Callback do
   A callback is a piece of code which implements some functionality of a
   `Skitter.Component`. Internally, a callback is defined as an anonymous
   function and some metadata.
+
+  This module also contains `defcallback/4`, a DSL which can be used to define
+  callbacks.
   """
-  alias Skitter.Port
-  alias Skitter.Component.State
+  alias Skitter.{Component, Port, DSL, DefinitionError}
 
   defstruct [:function, :state_capability, :publish_capability]
 
@@ -46,17 +48,47 @@ defmodule Skitter.Component.Callback do
   @type publish :: [{Port.t(), any()}]
 
   @typedoc """
+  Structure of the component state.
+
+  When a callback is executed, it may access to the state of a component
+  instance (see `t:state_capability/0`). At the end of the invocation of the
+  callback, the (possibly updated) state is returned as part of the result of
+  the callback.
+
+  This state is represented as a map, where each key corresponds to a
+  `t:Component.field/0`. The value for each key corresponds to the current
+  value of the field.
+  """
+  @type state :: %{optional(Component.field()) => any}
+
+  defmodule Result do
+    @moduledoc """
+    Struct returned by a successful component callback.
+
+    A callback returns the following:
+    - The updated state, or `nil` if the state has not been changed.
+    - The published data or `nil` if no data has been published.
+    - The result of the callback
+    """
+    alias Skitter.Component.Callback
+
+    defstruct [:state, :publish, :result]
+
+    @type t :: %__MODULE__{
+            state: Callback.state(),
+            publish: Callback.publish(),
+            result: any()
+          }
+  end
+
+  @typedoc """
   Result returned by the invocation of a callback.
 
-  A successful callback is a four tuple which starts with `:ok`. Besides `:ok`,
-  the callback returns the new state of the component instance, the data to be
-  published and the result of the callback. `nil` can be provided instead of
-  the state or published data if the callback does not update the instance
-  state, or if it does not publish any data.
+  A successful callback returns a `t:Result.t/0` struct, which contains the
+  updated state, published data and result value of the callback.
   When not successful, the callback returns an `{:error, reason}` tuple.
   """
-  @type result ::
-          {:ok, State.t() | nil, publish() | nil, any()} | {:error, any()}
+  @type result :: Result.t() | {:error, any()}
 
   @typedoc """
   Function signature of a callback.
@@ -65,7 +97,7 @@ defmodule Skitter.Component.Callback do
   amount of arguments wrapped in a list. The return value is defined by
   `t:result/0`.
   """
-  @type signature :: (State.t(), [any()] -> result())
+  @type signature :: (state(), [any()] -> result())
 
   @typedoc """
   Defines how the callback may access the state.
@@ -76,18 +108,6 @@ defmodule Skitter.Component.Callback do
   Defines if the callback can publish data.
   """
   @type publish_capability :: boolean()
-
-  @doc """
-  Create a callback. Arguments are identical to `t:t/0`.
-  """
-  @spec create(signature(), state_capability(), publish_capability()) :: t()
-  def create(func, state_capability, publish_capability) do
-    %__MODULE__{
-      function: func,
-      state_capability: state_capability,
-      publish_capability: publish_capability
-    }
-  end
 
   @doc """
   Check if the callback adheres to its capabilities.
@@ -145,11 +165,225 @@ defmodule Skitter.Component.Callback do
 
   ## Examples
 
-      iex> cb = %Callback{
-      ...>  function: fn s, [a, b] -> {:ok, s, [out: s.f + a], b} end}
-      iex> call(cb, %{f: 1}, [2, 3])
-      {:ok, %{f: 1}, [out: 3], 3}
+      iex> cb = defcallback([:field], [:out], [arg1, arg2]) do
+      ...>  field <~ field + arg1
+      ...>  arg2 ~> out
+      ...>  field
+      ...> end
+      iex> call(cb, %{field: 1}, [2, 3])
+      %Result{state: %{field: 3}, publish: [out: 3], result: 3}
   """
   @spec call(t(), State.t(), [any()]) :: result()
   def call(%__MODULE__{function: f}, state, args), do: f.(state, args)
+
+  # ------ #
+  # Macros #
+  # ------ #
+
+  @publish_operator :~>
+  @update_operator :<~
+
+  @publish_var DSL.create_internal_var(:publish)
+  @state_var DSL.create_internal_var(:state)
+
+  @doc """
+  DSL to create skitter callbacks.
+
+  This macro offers a small DSL to create callbacks that can be used inside
+  skitter components. This macro accepts the `fields` and `out_ports` of the
+  component it will be embedded in as arguments. Any additional arguments that
+  the callback will accept must be specified as a list of arguments which
+  should be passed to `args`.
+
+  The `body` argument should contain the implementation of the callback. The
+  body of the callback contains standard elixir code with a few additions and
+  limitations:
+
+  - the `~>` operator can be used to publish data to any out port provided in
+  `out_ports` (an error is raised if the port is not present in `out_ports`).
+  This is done with the following syntax: `value ~> port`. If multiple data is
+  published on the same output port, the last write will be published.
+  - the `<~` operator can be used to update the state passed to the callback.
+  `field <~ value` will update `field` of the state. The provided field should
+  be present in `fields` (an error is raised  if this is not the case).
+  - Using any field name inside of `body` will read the current value of that
+  field in the state passed to the callback activation. Thus, to avoid errors,
+  variable names inside `body` should not have the same name as a field.
+
+  As a result, this macro returns a callback with the correct capabilities and a
+  function which will automatically return a `t:Result.t()` struct.
+
+  ## Examples
+
+  This callback calculates an average and publishes its current value on the
+  `current` port. For educative reasons, it always returns 0.
+
+      iex> c = defcallback([:total, :count], [:current], [value]) do
+      ...>  count <~ count + 1
+      ...>  total <~ total + value
+      ...>  total / count ~> current
+      ...>  0
+      ...> end
+      iex> c.publish_capability
+      true
+      iex> c.state_capability
+      :readwrite
+      iex> call(c, %{total: 5, count: 1}, [5])
+      %Result{state: %{count: 2, total: 10}, publish: [current: 5.0], result: 0}
+  """
+  defmacro defcallback(fields, out_ports, args, do: body) do
+    try do
+      body = transform_operators(fields, out_ports, body, __CALLER__)
+
+      state = state_access(used?(body, :read_state), used?(body, :update_state))
+      publish = used?(body, :publish)
+
+      {body, state_return, state_arg} = make_state_body_return(body, state)
+      {body, publish_return} = make_publish_body_return(body, publish)
+
+      func =
+        quote do
+          fn unquote(state_arg), unquote(args) ->
+            import unquote(__MODULE__),
+              only: [read_state: 1, update_state: 2, publish: 2]
+
+            result = unquote(body)
+
+            %unquote(__MODULE__.Result){
+              state: unquote(state_return),
+              publish: unquote(publish_return),
+              result: result
+            }
+          end
+        end
+
+      quote do
+        %unquote(__MODULE__){
+          function: unquote(func),
+          state_capability: unquote(state),
+          publish_capability: unquote(publish)
+        }
+      end
+    catch
+      err -> handle_error(err)
+    end
+  end
+
+  @doc false
+  defmacro publish(port, value) do
+    quote do
+      unquote(@publish_var) =
+        Keyword.put(unquote(@publish_var), unquote(port), unquote(value))
+    end
+  end
+
+  @doc false
+  defmacro update_state(field, value) do
+    quote do
+      unquote(@state_var) = %{
+        unquote(@state_var)
+        | unquote(field) => unquote(value)
+      }
+    end
+  end
+
+  @doc false
+  defmacro read_state(field) do
+    quote(do: unquote(@state_var)[unquote(field)])
+  end
+
+  # Makes the following changes to the body:
+  #   - any variable with a name in `fields` is transformed into
+  #   `read_state(field)`
+  #   - Any use of <~ is transformed into `update_state(field, value)`, check
+  #   if field exists.
+  #   - Any use of the ~> operator is transformed into `publish(value, port)`,
+  #   check if port exists.
+  defp transform_operators(fields, out_ports, body, env) do
+    Macro.prewalk(body, fn
+      node = {name, _, atom} when is_atom(atom) ->
+        if(name in fields, do: quote(do: read_state(unquote(name))), else: node)
+
+      {@update_operator, _, [field, value]} ->
+        field = DSL.name_to_atom(field, env)
+
+        if field in fields do
+          quote(do: update_state(unquote(field), unquote(value)))
+        else
+          throw {:error, :invalid_field, field, fields, env}
+        end
+
+      {@publish_operator, _, [value, port]} ->
+        port = DSL.name_to_atom(port, env)
+
+        if port in out_ports do
+          quote(do: publish(unquote(port), unquote(value)))
+        else
+          throw {:error, :invalid_out_port, port, out_ports, env}
+        end
+
+      any ->
+        any
+    end)
+  end
+
+  # Check if `symbol` is used in `ast`
+  defp used?(ast, symbol) do
+    {_, n} =
+      Macro.prewalk(ast, 0, fn
+        ast = {^symbol, _env, _args}, acc -> {ast, acc + 1}
+        ast, acc -> {ast, acc}
+      end)
+
+    n >= 1
+  end
+
+  # Check state access based on read / write
+  defp state_access(_, true), do: :readwrite
+  defp state_access(true, false), do: :read
+  defp state_access(false, false), do: :none
+
+  # Create body and return value and function argument for state
+  defp make_state_body_return(body, :readwrite) do
+    {DSL.make_mutable_in_block(body, @state_var), @state_var, @state_var}
+  end
+
+  defp make_state_body_return(body, :none) do
+    {body, nil, DSL.create_internal_var(:_)}
+  end
+
+  defp make_state_body_return(body, :read) do
+    {body, nil, @state_var}
+  end
+
+  # Create body and return value for publishing values
+  defp make_publish_body_return(body, true) do
+    body = DSL.make_mutable_in_block(body, @publish_var)
+
+    body =
+      quote do
+        unquote(@publish_var) = []
+        unquote(body)
+      end
+
+    {body, @publish_var}
+  end
+
+  defp make_publish_body_return(body, false), do: {body, nil}
+
+  defp handle_error({:error, :invalid_syntax, statement, env}) do
+    DefinitionError.inject("Invalid syntax: `#{statement}`", env)
+  end
+
+  defp handle_error({:error, :invalid_field, field, fields, env}) do
+    DefinitionError.inject(
+      "Invalid field: `#{field}` is not a part of `#{inspect fields}`", env
+    )
+  end
+
+  defp handle_error({:error, :invalid_out_port, port, out_ports, env}) do
+    DefinitionError.inject(
+      "Invalid out port: `#{port}` is not a part of `#{inspect out_ports}`", env
+    )
+  end
 end
--- test/component/callback_test.exs
+++ test/component/callback_test.exs
@@ -6,21 +6,88 @@
 
 defmodule Skitter.Component.CallbackTest do
   use ExUnit.Case, async: true
+  import Skitter.Test.Assertions
+
   alias Skitter.Component.Callback
+  alias Skitter.Component.Callback.Result
+
   import Skitter.Component.Callback
   doctest Skitter.Component.Callback
 
-  test "create/3" do
-    f = fn state, [_] -> {:ok, state, nil} end
+  describe "defcallback" do
+    test "`:none` state capability" do
+      c = defcallback([:field], [], []) do
+        10
+      end
 
-    func = create(f, :read, false)
+      assert c.state_capability == :none
+      assert call(c, %{field: nil}, []).state == nil
+    end
 
-    struct = %Callback{
-      function: f,
-      state_capability: :read,
-      publish_capability: false
-    }
+    test "`:read` state capability" do
+      c = defcallback([:field], [], []) do
+        field
+      end
 
-    assert func == struct
+      assert c.state_capability == :read
+
+      res = call(c, %{field: 50}, [])
+      assert res.state == nil
+      assert res.result == 50
+    end
+
+    test "`:readwrite` state capability" do
+      c = defcallback([:field], [], []) do
+        field <~ 30
+      end
+
+      assert c.state_capability == :readwrite
+      assert call(c, %{field: nil}, []).state == %{field: 30}
+    end
+
+    test "no publish" do
+      c = defcallback([], [:out], []) do
+        5
+      end
+
+      assert c.publish_capability == false
+      assert call(c, %{}, []).publish == nil
+    end
+
+    test "publish" do
+      c = defcallback([], [:out], []) do
+        5 ~> out
+      end
+
+      assert c.publish_capability == true
+      assert call(c, %{}, []).publish == [out: 5]
+    end
+
+    test "arguments" do
+      c1 = defcallback([], [], [arg]) do
+        arg
+      end
+
+      c2 = defcallback([], [], [arg1, arg2]) do
+        arg1 + arg2
+      end
+
+      assert call(c1, %{}, [10]).result == 10
+      assert call(c2, %{}, [10, 20]).result == 30
+    end
+
+    test "errors" do
+      assert_definition_error ~r/.*: Invalid syntax: `foo`/ do
+        defcallback([], [], [], do: 5 ~> :foo)
+      end
+
+      assert_definition_error ~r/.*: Invalid field: .*/ do
+        defcallback([:field], [], [], do: another_field <~ 5)
+      end
+
+      assert_definition_error ~r/.*: Invalid out port: .*/ do
+        defcallback([], [:out], [], do: 5 ~> wrong_port)
+      end
+    end
   end
 end
