ab05f1865bfb98ad5a9b6c82e0d458a288ab2b78
Mathijs Saey <mathsaey@vub.ac.be>
2019-07-02 14:58:58 +0200

    adjust formatter for new dsls

--- .formatter.exs
+++ .formatter.exs
@@ -3,21 +3,12 @@
   line_length: 80,
   inputs: ["mix.exs", "{config,lib,test}/**.{ex,exs}"],
   locals_without_parens: [
-    # General
     throw: :*,
-    inject_error: :*,
-    # Component DSL
-    spit: :*,
-    throw: :*,
-    error: :*,
-    effect: :*,
-    fields: :*,
-    state: :*,
-    component: :*,
-    state_change: :*,
-    external_effect: :*,
-    # Workflow DSL
+    # DSL
+    defcomponent: :*,
+    defworkflow: :*,
     instance: :*,
+    fields: :*,
     # Logger
     info: :*,
     debug: :*,
