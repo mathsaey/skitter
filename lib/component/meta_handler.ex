c2607011dce4f9c6ea815670ac7195f7d118507d
Mathijs Saey <mathsaey@vub.ac.be>
2019-07-05 23:37:30 +0200

    Add default on_define

--- lib/component/meta_handler.ex
+++ lib/component/meta_handler.ex
@@ -8,9 +8,11 @@ defmodule Skitter.Component.MetaHandler do
   @moduledoc false
 
   import Skitter.Component
+  import Skitter.Component.Callback, only: [defcallback: 4]
 
-  def on_define(component) do
-    component
+  def on_define(comp) do
+    comp
+    |> default_callback(:on_define, defcallback([], [], [c], do: c))
     |> require_callback(:on_define, arity: 1)
   end
 end
