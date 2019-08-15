26bc5cfcd2b725f650a565250fe84df2c08e03b8
Mathijs Saey <mathsaey@vub.ac.be>
2019-08-15 21:07:36 +0200

    update ex_doc

--- mix.exs
+++ mix.exs
@@ -34,7 +34,7 @@ defmodule Skitter.MixProject do
     [
       # Dev tools
       {:credo, "~> 1.0.0", only: :dev, runtime: false},
-      {:ex_doc, "~> 0.20.0", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false},
       {:dialyxir, "~> 1.0.0-rc.6", only: :dev, runtime: false}
     ]
   end
@@ -47,6 +47,7 @@ defmodule Skitter.MixProject do
       groups_for_modules: [
         "Language Concepts": [
           Skitter.Port,
+          Skitter.Element,
           Skitter.Instance,
           ~r/Skitter\.Component.*/,
           ~r/Skitter\.Workflow.*/
