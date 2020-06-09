1734e035381e4c6cd53f79c6ee64cfe2d7a4c367
Mathijs Saey <mathsaey@vub.ac.be>
2020-06-09 22:29:08 +0200

    Rename copy pasted module name

--- apps/skitter_master/test/workers_test.exs
+++ apps/skitter_master/test/workers_test.exs
@@ -4,7 +4,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-defmodule Skitter.Worker.MasterTest do
+defmodule Skitter.Master.WorkersTest do
   use Skitter.Runtime.Test.ClusterCase, async: false
 
   alias Skitter.Master.Workers
