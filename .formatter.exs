310ab408d171ba8df12682c2c9011bf12dd40ea4
Mathijs Saey <mathsaey@vub.ac.be>
2020-09-02 19:33:01 +0200

    Move formatter config to single file

--- .formatter.exs
+++ .formatter.exs
@@ -4,26 +4,29 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-# Use the same formatting rules for all applications which are not specifically
-# excluded.
-
-exlude = [:dsl]
-
-inputs =
-  Mix.Project.apps_paths()
-  |> Enum.reject(fn {key, _} -> key in exlude end)
-  |> Enum.map(fn {_, path} -> "#{path}/{lib,test}/**/*.{ex, exs}" end)
-  |> (fn inputs -> ["mix.exs", "config/*.exs"] ++ inputs end).()
-
-subdirectories =
-  Mix.Project.apps_paths()
-  |> Enum.filter(fn {key, _} -> key in exlude end)
-  |> Enum.map(fn {_, path} -> path end)
-
 [
-  inputs: inputs,
-  subdirectories: subdirectories,
+  inputs: [
+    "mix.exs",
+    ".formatter.exs",
+    "config/*.exs",
+    "apps/*/mix.exs",
+    "apps/*/{lib,test}/**/*.{ex, exs}"
+  ],
   locals_without_parens: [
+    # Elixir
     throw: :*,
+    # Logger
+    info: :*,
+    debug: :*,
+    warn: :*,
+    error: :*,
+    # Skitter DSL
+    defcomponent: :*,
+    defworkflow: :*,
+    strategy: :*,
+    instance: :*,
+    fields: :*,
+    # Skitter runtime
+    config_from_env: :*
   ]
 ]
