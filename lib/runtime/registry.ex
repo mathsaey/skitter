6fb7792358a0c00e4e44c52b995c8b370cfbe959
Mathijs Saey <mathsaey@vub.ac.be>
2021-06-16 17:32:53 +0200

    Add tag support to registry

--- lib/runtime/registry.ex
+++ lib/runtime/registry.ex
@@ -8,19 +8,47 @@ defmodule Skitter.Runtime.Registry do
   @moduledoc false
   alias Skitter.Remote
 
+  @node_tab __MODULE__.Nodes
+  @tag_tab __MODULE__.Tags
+
   def start_link do
-    case :ets.new(__MODULE__, [:set, :protected, :named_table, {:read_concurrency, true}]) do
-      __MODULE__ -> :ok
+    with @node_tab <- :ets.new(@node_tab, [:named_table, {:read_concurrency, true}]),
+         @tag_tab <- :ets.new(@tag_tab, [:bag, :named_table, {:read_concurrency, true}]) do
+      :ok
     end
   end
 
-  def add(node), do: :ets.insert_new(__MODULE__, {node, nil})
-  def remove(node), do: :ets.delete(__MODULE__, node)
-  def remove_all(), do: :ets.delete_all_objects(__MODULE__)
+  def add(node, tags \\ []) do
+    :ets.insert_new(@node_tab, {node})
+    Enum.each(tags, &:ets.insert(@tag_tab, {&1, node}))
+  end
 
-  def all, do: :ets.tab2list(__MODULE__) |> Enum.map(&elem(&1, 0))
-  def connected?(node), do: :ets.member(__MODULE__, node)
+  def remove(node) do
+    :ets.delete(@node_tab, node)
+    :ets.match_delete(@tag_tab, {:"$_", node})
+  end
+
+  def remove_all() do
+    :ets.delete_all_objects(@node_tab)
+    :ets.delete_all_objects(@tag_tab)
+  end
+
+  def all, do: :ets.tab2list(@node_tab) |> Enum.map(&elem(&1, 0))
+  def connected?(node), do: :ets.member(@node_tab, node)
+  def with_tag(tag), do: :ets.match(@tag_tab, {tag, :"$1"}) |> Enum.map(&hd/1)
+
+  def tags(node) do
+    case :ets.match(@tag_tab, {:"$1", node}) do
+      [] -> raise KeyError
+      lst -> Enum.map(lst, &hd/1)
+    end
+  end
+
+  def all_with_tags, do: Enum.map(all(), fn node -> {node, tags(node)} end)
 
   def on_all(mod, func, args), do: Remote.on_many(all(), mod, func, args)
   def on_all(fun), do: Remote.on_many(all(), fun)
+
+  def on_tagged(tag, mod, func, args), do: tag |> with_tag() |> Remote.on_many(mod, func, args)
+  def on_tagged(tag, fun), do: tag |> with_tag() |> Remote.on_many(fun)
 end
