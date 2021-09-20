8f5dc77d6089590569b46318dee8a031102d4f83
Mathijs Saey <mathsaey@vub.ac.be>
2021-09-20 18:59:22 +0200

    Add additional ways to distribute work over nodes

--- lib/nodes.ex
+++ lib/nodes.ex
@@ -6,49 +6,24 @@
 
 defmodule Skitter.Nodes do
   @moduledoc """
-  Remote Skitter runtimes.
+  Facilities to interact with Skitter worker nodes.
+
+  Skitter workers nodes are remote Skitter runtimes which are responsible for performing work for
+  a Skitter application. This module offers functions to query the runtime system about the
+  available Skitter worker runtimes and their tags. It also defines various functions, such as
+  `on_all_worker_cores/1` which can be used to spawn Skitter workers distributed over the cluster
+  in various different configurations.
   """
   alias Skitter.Runtime.Registry
 
   @typedoc """
-  Node tag.
+  Worker tag.
 
-  A node may be tagged with atoms which indicate properties of the node.
+  A worker may be started with a tag, which indicates properties of the node. For instance, a
+  `:gpu` tag could be added to a node which has a gpu. Various functions in this module can be
+  used to only spawn workers on nodes with given tags.
   """
   @type tag :: atom()
-
-  @doc """
-  Execute a function on every worker node.
-
-  The result of each worker will be returned in a keyword list of `{worker, result}` pairs.
-  """
-  @spec on_all_workers((() -> any())) :: [{node(), any()}]
-  def on_all_workers(fun), do: Registry.on_all(fun)
-
-  @doc """
-  Execute a function on every worker node with a given `tag`.
-
-  The result of each worker will be returned in a keyword list of `{worker, result}` pairs.
-  """
-  @spec on_tagged_workers(tag(), (() -> any())) :: [{node(), any()}]
-  def on_tagged_workers(tag, fun), do: Registry.on_tagged(tag, fun)
-
-  @doc """
-  Execute a function for every core on the current node.
-
-  This can be useful to spawn a worker per core.
-  """
-  @spec on_all_cores((() -> any())) :: [any()]
-  def on_all_cores(fun), do: Enum.map(1..System.schedulers_online(), fn _ -> fun.() end)
-
-  @doc """
-  Execute a function on every core on every worker node.
-
-  A list of results will be returned for each worker node. These results will be returned in a
-  keyword list of `{worker, result}` pairs.
-  """
-  @spec on_all_worker_cores((() -> any())) :: [{node(), [any()]}]
-  def on_all_worker_cores(fun), do: on_all_workers(fn -> on_all_cores(fun) end)
 
   @doc """
   Get the name of the current node.
@@ -57,13 +32,13 @@ defmodule Skitter.Nodes do
   def self(), do: Node.self()
 
   @doc """
-  Get a list of all the workers in the cluster.
+  Get a list of the names of all the worker runtimes in the cluster.
   """
   @spec workers() :: [node()]
   def workers(), do: Registry.all()
 
   @doc """
-  Get a list of all workers with a `t:tag/0`
+  Get a list of all the worker runtimes tagged with a given `t:tag/0`.
   """
   @spec with_tag(tag()) :: [node()]
   def with_tag(tag), do: Registry.with_tag(tag)
@@ -73,4 +48,71 @@ defmodule Skitter.Nodes do
   """
   @spec tags(node()) :: [tag()]
   def tags(node), do: Registry.tags(node)
+
+  @doc """
+  Execute a function on every worker runtime.
+
+  The result of each worker will be returned in a keyword list of `{worker, result}` pairs.
+  """
+  @spec on_all_workers((() -> any())) :: [{node(), any()}]
+  def on_all_workers(fun), do: Registry.on_all(fun)
+
+  @doc """
+  Execute a function on every core on every worker runtime.
+
+  A list of results will be returned for each worker node. These results will be returned in a
+  keyword list of `{worker, result}` pairs.
+  """
+  @spec on_all_worker_cores((() -> any())) :: [{node(), [any()]}]
+  def on_all_worker_cores(fun), do: on_all_workers(fn -> core_times(fun) end)
+
+  @doc """
+  Execute a function n times on every worker runtime.
+
+  A list of results will be returned for each worker node. These results will be returned in a
+  keyword list of `{worker, result}` pairs.
+  """
+  @spec n_times_on_all_workers((() -> any()), pos_integer()) :: [{node(), [any()]}]
+  def n_times_on_all_workers(fun, n), do: on_all_workers(fn -> n_times(fun, n) end)
+
+  @doc """
+  Execute a function on every worker runtime tagged with `tag`.
+
+  The result of each worker will be returned in a keyword list of `{worker, result}` pairs.
+  """
+  @spec on_tagged_workers(tag(), (() -> any())) :: [{node(), any()}]
+  def on_tagged_workers(tag, fun), do: Registry.on_tagged(tag, fun)
+
+  @doc """
+  Execute a function on every core on every worker runtime tagged with `tag`.
+
+  The result of each worker will be returned in a keyword list of `{worker, result}` pairs.
+  """
+  @spec on_tagged_worker_cores(tag(), (() -> any())) :: [{node(), any()}]
+  def on_tagged_worker_cores(tag, fun), do: Registry.on_tagged(tag, fn -> core_times(fun) end)
+
+  @doc """
+  Execute a function n times, distributed over the available workers.
+
+  This is handy when you wish to create n workers distributed over the cluster. The work to be
+  done will be divided over the worker nodes in a round robin fashion. This behaviour may change
+  in the future.
+  """
+  @spec on_n((() -> any()), pos_integer()) :: [[any()]]
+  def on_n(fun, n) do
+    workers()
+    |> Enum.shuffle()
+    |> Stream.cycle()
+    |> Enum.take(n)
+    |> Enum.frequencies()
+    |> Enum.flat_map(fn {remote, times} ->
+      Skitter.Remote.on(remote, fn -> n_times(fun, times) end)
+    end)
+  end
+
+  @spec n_times((() -> any()), pos_integer()) :: [any()]
+  defp n_times(fun, n), do: Enum.map(1..n, fn _ -> fun.() end)
+
+  @spec core_times((() -> any())) :: [any()]
+  defp core_times(fun), do: n_times(fun, System.schedulers_online())
 end
