d488adf7a0eeaa92307177cdb7694f1e1e894149
Mathijs Saey <mathsaey@vub.ac.be>
2022-02-16 20:36:00 +0100

    bump version number after release

--- mix.exs
+++ mix.exs
@@ -15,7 +15,7 @@ defmodule Skitter.MixProject do
       app: :skitter,
       name: "Skitter",
       elixir: "~> 1.13",
-      version: "0.5.0",
+      version: "0.5.1-dev",
       source_url: @github_url,
       homepage_url: @home_url,
       start_permanent: Mix.env() == :prod,
