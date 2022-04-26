1095027238bfa01cecc23aa9e18ec96992ee59a5
Mathijs Saey <mathsaey@vub.ac.be>
2022-04-26 20:25:38 +0200

    Do not run external tests by default

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -2,4 +2,6 @@
 Code.put_compiler_option(:ignore_module_conflict, true)
 # Avoid "Application has been stopped" messages
 Logger.put_module_level(:application_controller, :error)
+# Do not run remote test by default
+ExUnit.configure(exclude: [remote: true])
 ExUnit.start()
