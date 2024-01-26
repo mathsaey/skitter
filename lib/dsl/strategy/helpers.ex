b0d68b960e9c80e38810f1f5e8d4999c98ca8d31
Mathijs Saey <mathsaey@vub.ac.be>
2024-01-26 16:48:59 +0100

    typo

--- lib/dsl/strategy/helpers.ex
+++ lib/dsl/strategy/helpers.ex
@@ -61,7 +61,7 @@ defmodule Skitter.DSL.Strategy.Helpers do
   end
 
   @doc """
-  Send a message to a worker using the Elixir's `Kernel.send/2`.
+  Send a message to a worker using Elixir's `Kernel.send/2`.
 
   `send/2` and `send/3` defined in this module call `Skitter.Worker.send/2` which will send a
   message to a worker, eventually causing its `c:Skitter.Strategy.Operation.process/4` hook to be
