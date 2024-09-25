ea66c3814f64391aeaa2fc4feaed4e337041c65d
Mathijs Saey <mathsaey@vub.ac.be>
2024-09-25 20:48:21 +0200

    Fix worker initialization race condition

--- lib/runtime/worker.ex
+++ lib/runtime/worker.ex
@@ -7,6 +7,26 @@
 defmodule Skitter.Runtime.Worker do
   @moduledoc """
   This module defines a GenServer that specifies the behaviour of Skitter Workers.
+
+  ## Worker Initialisation
+
+  Workers that are created inside the deploy hook of a strategy may not perform any processing
+  until they receive the `:sk_deploy_complete` message. This is done to avoid any processing being
+  done before the entire workflow has finished deployment. It also ensures that the emit hook
+  of downstream nodes in the workflow can not be called before their deployment is finished.
+
+  The Skitter runtime guarantees that the `:sk_deploy_complete` message is sent in reverse
+  topological order so that all the nodes downstream of a node are ready to receive data once a
+  node activates.
+
+  Before the deployment of a workflow is complete, the Skitter runtime sets the `_skr` field of
+  the `t:Skitter.Strategy.Context/0` to `{:deploy, _, _}`. Thus, when a worker is spawned, it
+  checks the value of this field to see if it can finish initialisation.
+
+  When a strategy spawns several workers that communicate with each other, it is possible that
+  workers do receive a message before they receive the `:_sk_deploy_complete` message. In this
+  case, the worker is initialized on the spot and any future `:_sk_deploy_complete` messages are
+  ignored.
   """
   use GenServer, restart: :transient
 
@@ -20,20 +40,36 @@ defmodule Skitter.Runtime.Worker do
   def deploy_complete(pid), do: GenServer.cast(pid, :sk_deploy_complete)
 
   @impl true
-  def init({context = %{_skr: {:deploy, _, _}}, state, tag}), do: {:ok, {context, state, tag}}
-  def init({context, state, tag}), do: {:ok, init_state({context, state, tag})}
+  def init({context = %{_skr: {:deploy, _, _}}, state, tag}) do
+    {:ok, {:uninitialized, context, state, tag}}
+  end
+
+  def init({context, state, tag}) do
+    {:ok, init_state({context, state, tag})}
+  end
 
   @impl true
-  def handle_cast(:sk_deploy_complete, {context, state, tag}) do
-    context = update_in(context._skr, fn {:deploy, ref, idx} -> {ref, idx} end)
-    {:noreply, init_state({context, state, tag})}
+  def handle_cast(:sk_deploy_complete, state = {:uninitialized, _, _, _}) do
+    {:noreply, activate_worker(state)}
+  end
+
+  def handle_cast(:sk_deploy_complete, srv), do: {:noreply, srv}
+
+  def handle_cast({:sk_msg, msg}, state = {:uninitialized, _, _, _}) do
+    srv = activate_worker(state)
+    {:noreply, process_hook(msg, srv)}
   end
 
   def handle_cast({:sk_msg, msg}, srv), do: {:noreply, process_hook(msg, srv)}
-  def handle_cast(:sk_stop, srv), do: {:stop, :normal, srv}
+  def handle_cast(:sk_stop, state), do: {:stop, :normal, state}
 
   @impl true
   def handle_info(msg, srv), do: {:noreply, process_hook(msg, srv)}
+
+  defp activate_worker({:uninitialized, context, state, tag}) do
+    context = update_in(context._skr, fn {:deploy, ref, idx} -> {ref, idx} end)
+    init_state({context, state, tag})
+  end
 
   defp init_state({context, state, tag}) when is_function(state, 0) do
     init_state({context, state.(), tag})
