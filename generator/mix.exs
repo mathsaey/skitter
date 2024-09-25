00fc75a43b0d848f5f3cf732e920a5f0ba262f87
Mathijs Saey <mathsaey@vub.ac.be>
2024-09-25 20:48:52 +0200

    version bump

--- generator/mix.exs
+++ generator/mix.exs
@@ -14,7 +14,7 @@ defmodule SkitterNew.MixProject do
     [
       app: :skitter_new,
       elixir: "~> 1.16",
-      version: "0.7.0",
+      version: "0.7.1",
       source_url: @github_url,
       homepage_url: @home_url,
       start_permanent: Mix.env() == :prod,
